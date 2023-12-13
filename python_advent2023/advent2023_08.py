ca570f0d79bcf6eaf2be80be60c2562249f501cf
funlikely <smJU.20tackom$eat>
2023-12-13 13:44:14 -0500

    Day 8 Part 2 Solution

--- python_advent2023/advent2023_08.py
+++ python_advent2023/advent2023_08.py
@@ -4,7 +4,9 @@
     Starting at AAA, follow the left/right instructions. How many steps are required to reach ZZZ?
 
 """
+import math
 import time
+from typing import List
 
 debug1 = True
 
@@ -19,7 +21,7 @@ def read_file(file_path):
 
 
 def get_graph(graph_data):
-    graph = {line[0:3]:({'L':line[7:10], 'R':line[12:15]}) for line in graph_data}
+    graph = {line[0:3]: ({'L': line[7:10], 'R': line[12:15]}) for line in graph_data}
     return graph
 
 
@@ -43,9 +45,35 @@ def get_answer_1():
     return steps
 
 
+def get_lcm(nums: List[int]) -> int:
+    if len(nums) == 1:
+        return nums[0]
+    return get_lcm([math.lcm(nums[0], nums[1])] + nums[2:])
+
+
 def get_answer_2():
     data = read_file('data/08.txt')
-    return 0
+
+    instructions = data[0]
+    graph = get_graph(data[2:])
+    if debug1:
+        print(f'instructions: {instructions}')
+        print(f'graph: {graph}')
+
+    a_starts = [k for k in graph.keys() if k[-1] == 'A']
+    steps_list = [0] * len(a_starts)
+    for i in range(len(a_starts)):
+        steps = 0
+        current = a_starts[i]
+        while current[-1] != 'Z':
+            direction = instructions[steps % len(instructions)]
+            if debug1:
+                print(f'at {current}, moving {direction} on {graph[current]}')
+            current = graph[current][direction]
+            steps += 1
+        steps_list[i] = steps
+
+    return get_lcm(steps_list)
 
 
 def main():
@@ -58,7 +86,7 @@ def main():
     print(f"The Answer to Advent of Code 2023, 08, 2 is '{answer_2}'")
 
     # The Answer to Advent of Code 2023, 08, 1 is '22411'
-    # The Answer to Advent of Code 2023, 08, 2 is
+    # The Answer to Advent of Code 2023, 08, 2 is '11188774513823'
 
 
 if __name__ == "__main__":
