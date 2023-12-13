a0907d47425a1c7529d9731ff0a6512bda4b4157
funlikely <smJU.20tackom$eat>
2023-12-13 13:44:14 -0500

    Day 8 Part 1 Solution

--- python_advent2023/advent2023_08.py
+++ python_advent2023/advent2023_08.py
@@ -18,13 +18,33 @@ def read_file(file_path):
     return lines
 
 
+def get_graph(graph_data):
+    graph = {line[0:3]:({'L':line[7:10], 'R':line[12:15]}) for line in graph_data}
+    return graph
+
+
 def get_answer_1():
-    data = read_file('data/07.txt')
-    return 0
+    data = read_file('data/08.txt')
+
+    instructions = data[0]
+    graph = get_graph(data[2:])
+    if debug1:
+        print(f'instructions: {instructions}')
+        print(f'graph: {graph}')
+
+    steps = 0
+    current = 'AAA'
+    while current != 'ZZZ':
+        direction = instructions[steps % len(instructions)]
+        if debug1:
+            print(f'at {current}, moving {direction} on {graph[current]}')
+        current = graph[current][direction]
+        steps += 1
+    return steps
 
 
 def get_answer_2():
-    data = read_file('data/07.txt')
+    data = read_file('data/08.txt')
     return 0
 
 
@@ -37,7 +57,7 @@ def main():
     print(f"The Answer to Advent of Code 2023, 08, 1 is '{answer_1}'")
     print(f"The Answer to Advent of Code 2023, 08, 2 is '{answer_2}'")
 
-    # The Answer to Advent of Code 2023, 08, 1 is
+    # The Answer to Advent of Code 2023, 08, 1 is '22411'
     # The Answer to Advent of Code 2023, 08, 2 is
 
 
