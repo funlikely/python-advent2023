fa9d17374993ed7f45f1bd707c7b843b302c66b4
funlikely <smJU.20tackom$eat>
2023-12-13 13:44:13 -0500

    Day 5 initial commit and problem setup

---
+++ python_advent2023/advent2023_06.py
@@ -0,0 +1,44 @@
+"""
+    --- Day 6: Wait For It ---
+
+    Determine the number of ways you could beat the record in each race. What do you get if you multiply these
+    numbers together?
+
+"""
+import time
+
+debug1 = True
+
+debug2 = True
+
+
+
+
+def get_answer_1():
+    data = ['Time:        55     99     97     93','Distance:   401   1485   2274   1405']
+
+    return 0
+
+
+def get_answer_2():
+    data = ['Time:        55     99     97     93','Distance:   401   1485   2274   1405']
+
+    return 0
+
+
+
+def main():
+    start = time.time()
+    answer_1 = get_answer_1()
+    answer_2 = get_answer_2()
+    end = time.time()
+    print(f"time taken: {end - start}")
+    print(f"The Answer to Advent of Code 2023, 06, 1 is '{answer_1}'")
+    print(f"The Answer to Advent of Code 2023, 06, 2 is '{answer_2}'")
+
+    # The Answer to Advent of Code 2023, 06, 1 is
+    # The Answer to Advent of Code 2023, 06, 2 is
+
+
+if __name__ == "__main__":
+    main()
