ba7491df16dcd4ced160c7eceb87f137e9812e51
funlikely <smJU.20tackom$eat>
2023-12-13 13:44:15 -0500

    Day 10 Part I solution

--- python_advent2023/advent2023_10.py
+++ python_advent2023/advent2023_10.py
@@ -28,9 +28,30 @@ directions = {'-': [{'y': 0,  'x': -1}, {'y': 0, 'x': 1}],
               #'S': [{'y': -1,  'x': 0}, {'y': 0, 'x': 1}]}  # special for now, since it acts as an 'L'
 
 
+data_file = 'data/10.txt'
+
+
+def print_picture(data, path):
+    picture = []
+    for j in range(len(data)):
+        line = ''.join([' ']*len(data[0]))
+        line = list(line)
+        picture.append(line)
+    for node in path:
+        picture[node[0]][node[1]] = data[node[0]][node[1]]
+    for line in picture:
+        if any([x for x in line if x != ' ']):
+            print(''.join(line))
+
+
 def get_answer_1():
-    data = read_file('data/10.txt')
-    distance = 0
+    data = read_file(data_file)
+    if data_file == 'data/10.txt':
+        directions['S'] = directions['L']
+    elif data_file == 'data/10_test.txt':
+        directions['S'] = directions['F']
+    elif data_file == 'data/10_test2.txt':
+        directions['S'] = directions['F']
 
     y, x = [(j, i) for j in range(len(data)) for i in range(len(data[0])) if data[j][i] == 'S'][0]
 
@@ -45,21 +66,48 @@ def get_answer_1():
             break
         y, x = next_node = next_nodes[0]
         path.append(next_node)
-        if len(path) % 1000 == 0:
+        if len(path) % 6 == 0 and len(path) < 200:
             print(f'Path length so far: {len(path)}')
+            print_picture(data, path)
         # print(f'path: {path}')
-    picture = [[' ']*len(data[0])] * len(data)
-    for node in path:
-        picture[node[0]][node[1]] = '*'
-    for line in picture:
-        print(''.join(line))
+    print_picture(data, path)
+    print(f'Total length of path: {len(path)}')
+    path_node_set = set(path)
+    print(f'Nodes in path are unique: {len(path) == len(path_node_set)}')
     return int(len(path) / 2)
 
 
 def get_answer_2():
-    data = read_file('data/10.txt')
-    total = 0
-    return total
+    data = read_file(data_file)
+    if data_file == 'data/10.txt':
+        directions['S'] = directions['L']
+    elif data_file == 'data/10_test.txt':
+        directions['S'] = directions['F']
+    elif data_file == 'data/10_test2.txt':
+        directions['S'] = directions['F']
+
+    y, x = [(j, i) for j in range(len(data)) for i in range(len(data[0])) if data[j][i] == 'S'][0]
+
+    path = [(y, x)]
+    print(f'loc: ({y},{x}), data(loc): {data[y][x]}')
+
+    while len(path) < 10 or data[y][x] != 'S':
+        choices = directions[data[y][x]]
+        adjacencies = [(y + choice['y'], x + choice['x']) for choice in choices]
+        next_nodes = [n for n in adjacencies if n not in path]
+        if not next_nodes:
+            break
+        y, x = next_node = next_nodes[0]
+        path.append(next_node)
+        if len(path) % 6 == 0 and len(path) < 200:
+            print(f'Path length so far: {len(path)}')
+            print_picture(data, path)
+        # print(f'path: {path}')
+    print_picture(data, path)
+    print(f'Total length of path: {len(path)}')
+    path_node_set = set(path)
+    print(f'Nodes in path are unique: {len(path) == len(path_node_set)}')
+    return int(len(path) / 2)
 
 
 def main():
@@ -71,7 +119,7 @@ def main():
     print(f"The Answer to Advent of Code 2023, 10, 1 is '{answer_1}'")
     print(f"The Answer to Advent of Code 2023, 10, 2 is '{answer_2}'")
 
-    # The Answer to Advent of Code 2023, 10, 1 is
+    # The Answer to Advent of Code 2023, 10, 1 is '6812'
     # The Answer to Advent of Code 2023, 10, 2 is
 
 
