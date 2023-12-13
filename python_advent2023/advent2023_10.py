2fb6ce2076b5e365ba354f465c29cd2fd0957d68
funlikely <smJU.20tackom$eat>
2023-12-13 13:44:15 -0500

    Day 10 Part 2 WIP

--- python_advent2023/advent2023_10.py
+++ python_advent2023/advent2023_10.py
@@ -30,15 +30,29 @@ directions = {'-': [{'y': 0,  'x': -1}, {'y': 0, 'x': 1}],
 
 data_file = 'data/10.txt'
 
+swapout = {'L': '╙', 'F': '╓', 'J': '╜', '7': '╖', '-': '─', '|': '║'}
 
-def print_picture(data, path):
+def get_picture(data, path):
     picture = []
     for j in range(len(data)):
-        line = ''.join([' ']*len(data[0]))
+        line = ''.join([' '] * len(data[0]))
         line = list(line)
         picture.append(line)
     for node in path:
         picture[node[0]][node[1]] = data[node[0]][node[1]]
+    for j in range(len(picture)):
+        for i in range(len(picture[0])):
+            if picture[j][i] in swapout.keys():
+                picture[j][i] = swapout[picture[j][i]]
+    return picture
+
+
+def get_and_print_picture(data, path):
+    picture = get_picture(data, path)
+    print_picture(picture)
+
+
+def print_picture(picture):
     for line in picture:
         if any([x for x in line if x != ' ']):
             print(''.join(line))
@@ -68,13 +82,34 @@ def get_answer_1():
         path.append(next_node)
         if len(path) % 6 == 0 and len(path) < 200:
             print(f'Path length so far: {len(path)}')
-            print_picture(data, path)
+            get_and_print_picture(data, path)
         # print(f'path: {path}')
-    print_picture(data, path)
+    get_and_print_picture(data, path)
     print(f'Total length of path: {len(path)}')
     path_node_set = set(path)
     print(f'Nodes in path are unique: {len(path) == len(path_node_set)}')
     return int(len(path) / 2)
+
+
+def mark_inner_nodes(picture):
+    outs = ['╖', '╜']
+    ins = ['╓', '╙']
+    ups = ['╙', '╜']
+    downs = ['╓', '╖']
+    for j in range(len(picture)):
+        inside = False
+        in_char = ''
+        for i in range(len(picture[0])):
+            if picture[j][i] == ' ':
+                picture[j][i] = 'I' if inside else 'O'
+            elif picture[j][i] in ins:
+                in_char = picture[j][i]
+            elif picture[j][i] in outs:
+                if picture[j][i] in ups and in_char in downs or picture[j][i] in downs and in_char in ups:
+                    inside = not inside
+            elif picture[j][i] == '║':
+                inside = not inside
+    return picture
 
 
 def get_answer_2():
@@ -99,15 +134,15 @@ def get_answer_2():
             break
         y, x = next_node = next_nodes[0]
         path.append(next_node)
-        if len(path) % 6 == 0 and len(path) < 200:
-            print(f'Path length so far: {len(path)}')
-            print_picture(data, path)
-        # print(f'path: {path}')
-    print_picture(data, path)
-    print(f'Total length of path: {len(path)}')
-    path_node_set = set(path)
-    print(f'Nodes in path are unique: {len(path) == len(path_node_set)}')
-    return int(len(path) / 2)
+
+    picture = get_picture(data, path)
+
+    picture = mark_inner_nodes(picture)
+    print_picture(picture)
+
+    enclosed = sum([1 for j in range(len(picture)) for i in range(len(picture[0])) if picture[j][i] == 'I'])
+
+    return enclosed
 
 
 def main():
@@ -121,6 +156,7 @@ def main():
 
     # The Answer to Advent of Code 2023, 10, 1 is '6812'
     # The Answer to Advent of Code 2023, 10, 2 is
+    # '543' IS TOO HIGH, apparently.
 
 
 if __name__ == "__main__":
