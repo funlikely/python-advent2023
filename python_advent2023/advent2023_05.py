7bee65636805599a5fa6308bb58b8b16ef753367
funlikely <smJU.20tackom$eat>
2023-12-13 13:44:12 -0500

    Day 5 wip 2

--- python_advent2023/advent2023_05.py
+++ python_advent2023/advent2023_05.py
@@ -6,8 +6,9 @@
 """
 import time
 import re
+from typing import Dict
 
-debug1 = False
+debug1 = True
 
 debug2 = True
 
@@ -24,23 +25,53 @@ def get_seeds(line):
 
 
 def get_maps(map_data):
-    maps = []
+    maps = [[]]
     for i in range(len(map_data)):
-        print(f'{i}')
+        if ':' in map_data[i]:
+            continue
+        if ' ' in map_data[i]:
+            map_line = [int(s) for s in map_data[i].split(' ')]
+            maps[-1].append(map_line)
+        if map_data[i] == '':
+            maps.append([])
     return maps
 
 
+def run_value_through_map(val, mapum):
+    for row in mapum:
+        if row[1] <= val < row[1] + row[2]:
+            return val - row[1] + row[0]
+    return val
+
+
+def get_locations_from_seed(seeds, maps):
+    locations = {}
+    for seed in seeds:
+        destination = seed
+        for mapum in maps:
+            destination = run_value_through_map(destination, mapum)
+        locations[seed] = destination
+    return locations
+
+
 def get_answer_1():
-    data = read_file('data/05.txt')
+    data = read_file('data/05_test.txt')
 
     seeds = get_seeds(data[0])
     if debug1:
         print(f'seed values: {seeds}')
 
     maps = get_maps(data[2:])
+    if debug1:
+        for map_item in maps:
+            print(f'map: {map_item}')
 
-    total = 0
-    return total
+    locations = get_locations_from_seed(seeds, maps)
+
+    min_location = min(locations, key=locations.get)
+    if debug1:
+        print(f'locations: {locations}')
+    return min_location
 
 
 def get_answer_2():
