bfc370df0b47dde9bf8263f9ef310d061066f959
funlikely <smJU.20tackom$eat>
2023-12-13 13:44:13 -0500

    Day 7 Part 1 problem setup

---
+++ python_advent2023/advent2023_07.py
@@ -0,0 +1,36 @@
+"""
+    --- Day 7: Camel Cards ---
+
+    Find the rank of every hand in your set. What are the total winnings?
+
+"""
+import time
+
+debug1 = True
+
+debug2 = True
+
+
+def get_answer_1():
+    return 0
+
+
+def get_answer_2():
+    return 0
+
+
+def main():
+    start = time.time()
+    answer_1 = get_answer_1()
+    answer_2 = get_answer_2()
+    end = time.time()
+    print(f"time taken: {end - start}")
+    print(f"The Answer to Advent of Code 2023, 07, 1 is '{answer_1}'")
+    print(f"The Answer to Advent of Code 2023, 07, 2 is '{answer_2}'")
+
+    # The Answer to Advent of Code 2023, 07, 1 is
+    # The Answer to Advent of Code 2023, 07, 2 is
+
+
+if __name__ == "__main__":
+    main()
