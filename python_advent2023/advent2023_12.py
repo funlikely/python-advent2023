bbfea6b28b7f94f869a6267774704d4e0c00055b
funlikely <ducksalt+github@gmail.com>
2023-12-14 21:34:57 -0500

    Day 12 Part 1 wip

--- python_advent2023/advent2023_12.py
+++ python_advent2023/advent2023_12.py
@@ -38,6 +38,32 @@ def get_combinations(slots, damaged):
         if len(slots) == 1:
             if g > len(slots):
                 return 0  # damaged group too big for available slot
+            if g == len(slots):
+                return 1  # just right
+            if g < len(slots):
+                if all([x == '?' for x in slots]):
+                    return len(slots) - g + 1  # the whole slot is wildcards
+                damaged_spots = [i for i,e in enumerate(slots) if e == '#']
+                required_damaged_span = max(damaged_spots) - min(damaged_spots) + 1
+                if required_damaged_span > g:
+                    raise ValueError(f'error in get_combinations({slots}, {damaged}')
+                elif required_damaged_span == g:
+                    return 1
+                elif slots[0] == '#':
+                    return 1
+                else:  # ??#??#??? g 5  ds 2 5  rds 4
+                    return len([(a, b) for a in range(len(slots)) for b in range(len(slots))
+                                if min(damaged_spots) <= a < b <= max(damaged_spots) and b - a + 1 == g])
+        return sum([get_combinations([slot], damaged) for slot in slots])
+    d0 = damaged[0]
+    s0 = slots[0]
+    if d0 > len(s0):
+        return get_combinations(slots, damaged[1:])
+    elif d0 == len(s0):
+        return get_combinations(slots[1:], damaged[1:]) + get_combinations(slots[1:], damaged)
+    else:
+        return 0
+
     return 0
 
 
