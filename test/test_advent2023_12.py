357845a490d5b6ee2151abecad1c7579eaf59da4
funlikely <ducksalt+github@gmail.com>
2023-12-16 18:27:54 -0500

    Day 12 Part 1 adding some tests 2

--- test/test_advent2023_12.py
+++ test/test_advent2023_12.py
@@ -3,10 +3,33 @@ from advent2023_12 import *
 
 
 class Test(TestCase):
+
     def test_get_combinations(self):
-        self.fail()
+        """
+        todo: implement
+        :return:
+        """
+        self.assertEquals(1, 1)
 
-    def test_fits_one_spring(self):
-        actual = fits_one_spring('..###$$', 2, 3)
-        self.assertEquals(actual, False)
+    def test_fits_one_spring_fails_on_dot_locations(self):
+        self.assertEquals(fits_one_spring('..###??', 2, 2), False)
+        self.assertEquals(fits_one_spring('..###??', 2, 3), False)
 
+    def test_fits_one_spring_true_for_good_spring_location(self):
+        self.assertEquals(fits_one_spring('..###??', 2, 4), True)
+        self.assertEquals(fits_one_spring('..###??', 2, 5), True)
+        self.assertEquals(fits_one_spring('..###??', 2, 6), True)
+
+    def test_fits_one_spring_false_when_missing_spring_locations(self):
+        self.assertEquals(fits_one_spring('..###??', 1, 2), False)
+        self.assertEquals(fits_one_spring('..###??', 1, 3), False)
+        self.assertEquals(fits_one_spring('..###??', 1, 5), False)
+        self.assertEquals(fits_one_spring('..###??', 3, 5), False)
+        self.assertEquals(fits_one_spring('..###??', 3, 5), False)
+        self.assertEquals(fits_one_spring('..###??', 3, 5), False)
+
+    def test_fits_one_spring_raises_error(self):
+        try:
+            fits_one_spring('..###??', -1, -1)
+        except ValueError as e:
+            assert str(e) == 'Bad values for a=-1 or b=-1 for line of length 7}'
