a6f43ac86ddc28ee314d6b2eade7dea7733a188b
funlikely <ducksalt+github@gmail.com>
2023-12-16 18:47:14 -0500

    Day 12 Part 1 adding some tests 2.5

--- test/test_advent2023_12.py
+++ test/test_advent2023_12.py
@@ -12,24 +12,30 @@ class Test(TestCase):
         self.assertEquals(1, 1)
 
     def test_fits_one_spring_fails_on_dot_locations(self):
-        self.assertEquals(fits_one_spring('..###??', 2, 2), False)
-        self.assertEquals(fits_one_spring('..###??', 2, 3), False)
+        self.assertEquals(fits_one_spring('..###??', 1, 2), False)
+        self.assertEquals(fits_one_spring('..###??', 1, 3), False)
+        self.assertEquals(fits_one_spring('..###??', 1, 4), False)
+        self.assertEquals(fits_one_spring('..###??', 1, 5), False)
 
     def test_fits_one_spring_true_for_good_spring_location(self):
+        self.assertEquals(fits_one_spring('..###??', 2, 3), False)
         self.assertEquals(fits_one_spring('..###??', 2, 4), True)
         self.assertEquals(fits_one_spring('..###??', 2, 5), True)
         self.assertEquals(fits_one_spring('..###??', 2, 6), True)
 
     def test_fits_one_spring_false_when_missing_spring_locations(self):
-        self.assertEquals(fits_one_spring('..###??', 1, 2), False)
-        self.assertEquals(fits_one_spring('..###??', 1, 3), False)
-        self.assertEquals(fits_one_spring('..###??', 1, 5), False)
         self.assertEquals(fits_one_spring('..###??', 3, 5), False)
         self.assertEquals(fits_one_spring('..###??', 3, 5), False)
         self.assertEquals(fits_one_spring('..###??', 3, 5), False)
 
-    def test_fits_one_spring_raises_error(self):
+    def test_fits_one_spring_raises_error_on_bad_a(self):
         try:
-            fits_one_spring('..###??', -1, -1)
+            fits_one_spring('..###??', -1, 2)
         except ValueError as e:
-            assert str(e) == 'Bad values for a=-1 or b=-1 for line of length 7}'
+            self.assertEquals(str(e), 'Bad values for a=-1 or b=2 for line of length 7')
+
+    def test_fits_one_spring_raises_error_on_bad_b(self):
+        try:
+            fits_one_spring('..###??', 1, 9)
+        except ValueError as e:
+            self.assertEquals(str(e), 'Bad values for a=1 or b=9 for line of length 7')
