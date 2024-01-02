7ff1f268446a37d016ace3f8af22d3fa2509c92c
funlikely <smJU.20tackom$eat>
2024-01-02 08:23:35 -0500

    Day 15 Part II WIP

--- python_advent2023/advent2023_15.py
+++ python_advent2023/advent2023_15.py
@@ -22,13 +22,28 @@ def read_file(file_path):
     return lines
 
 
-def run_hash_function(step):
+def run_hash_function2(step):
     acc = 0
     for c in step:
         acc += ord(c)
         acc *= 17
         acc %= 256
+    if debug1 and False:
+        print(f'hash({step} = {acc}')
     return acc
+
+
+def run_hash_function(step):
+    # using a fold to find the number of decrypted characters that are in the range of A-Z, a-z, and spaces
+    def f(acc, x):
+        return (acc + ord(x)) * 17 % 256
+
+    accumulator = 0
+    _ = [accumulator := f(accumulator, c) for c in step]
+
+    if debug1 and False:
+        print(f'hash({step}) = {accumulator}')
+    return accumulator
 
 
 def get_answer_1():
@@ -41,7 +56,31 @@ def get_code(step):
     if '-' in step:
         return {'label': step[:-1], 'box': run_hash_function(step[:-1]), 'action': 'remove'}
     else:
-        return {'label': step.split('=')[0], 'box': run_hash_function(step.split('=')[0]), 'action': 'add', 'lens':step.split('=')[1]}
+        return {'label': step.split('=')[0], 'box': run_hash_function(step.split('=')[0]), 'action': 'add',
+                'focal': step.split('=')[1]}
+
+
+boxes = [[]] * 256
+
+
+def add_lens(box, label, focal):
+    for lens in box:
+        if lens['label'] == label:
+            lens['focal'] = focal
+            return
+    box.append({'label': label, 'focal': focal})
+
+
+def remove_lens(box, label):
+    return [lens for lens in box if lens['label'] != label]
+
+
+def get_focusing_power_of_lens(i, lens):
+    return i * lens['focal']
+
+
+def get_focusing_power_of_box(i):
+    return (i + 1) * sum([get_focusing_power_of_lens(j + 1, boxes[i][j]) for j in range(len(boxes[i]))])
 
 
 def get_answer_2():
@@ -50,7 +89,15 @@ def get_answer_2():
     codes = [get_code(step) for step in steps]
     for step in steps:
         print(f'step: {step}, code: {get_code(step)}')
-    return 0
+
+    global boxes
+
+    for code in codes:
+        if code['action'] == 'add':
+            add_lens(code['box'], code['label'], code['focal'])
+        else:
+            remove_lens(code['box'], code['label'])
+    return sum([get_focusing_power_of_box(i) for i in range(len(boxes))])
 
 
 def main():
